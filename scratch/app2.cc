e90a5406ab27a09d3abaa969e2a5a3e3901146f8
Coaxial <Coaxial@localhost.localdomain>
2013-12-15 09:15:01 +0200

    commit

---
+++ scratch/app2.cc
@@ -0,0 +1,38 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/mod-module.h"
+
+using namespace ns3;
+
+int main(int argc ,char *argv[])
+{
+
+
+
+	 /*Config::SetDefault ("ns3::RateErrorModel::ErrorRate", DoubleValue (0.000));
+	  Config::SetDefault ("ns3::RateErrorModel::ErrorUnit", StringValue ("ERROR_UNIT_PACKET"));
+
+	  Config::SetDefault ("ns3::BurstErrorModel::ErrorRate", DoubleValue (0.00));
+	  Config::SetDefault ("ns3::BurstErrorModel::BurstSize", StringValue ("ns3::UniformRandomVariable[Min=1|Max=3]"))*/;
+
+	CommandLine c;
+	c.Parse (argc, argv);
+	//std::cout<<argv[1]<<std::endl;
+
+	//std::string temp="topology.txt";
+	Ptr<BootstrappingHelper> bh=CreateObject<BootstrappingHelper>(std::atoi(argv[3]),argv[1],std::atoi(argv[2]));
+	//Ptr<BootstrappingHelper> bh=CreateObject<BootstrappingHelper>(129,"1221.txt",50);
+
+	bh->parseTopology();
+
+	bh->startExperiment();
+
+	//bh=0;
+	//delete Text::getPtr();
+
+	Simulator::Destroy();
+	return 0;
+}
